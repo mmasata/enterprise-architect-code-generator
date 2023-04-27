d0981a7816aa7825de7f84670a573003aa23a382
masatma1 <masatma1@fel.cvut.cz>
2023-04-27 19:49:18 +0200

    Add avro-schema generator.

---
+++ generator-common/src/main/java/ea/code/generator/service/AvroSchemaGeneratorService.java
@@ -0,0 +1,37 @@
+package ea.code.generator.service;
+
+import ea.code.generator.annotations.GenerateCode;
+import ea.code.generator.annotations.RunGenerator;
+import ea.code.generator.context.GeneratorContext;
+import ea.code.generator.processor.FileProcessor;
+import ea.code.generator.service.mapper.AvroSchemaMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+import static ea.code.generator.service.constants.AvroSchemaConstants.AVRO_FREEMARKER_TEMPLATE_FILE;
+
+@Slf4j
+@GenerateCode(name = "avro-schema")
+@RequiredArgsConstructor
+public class AvroSchemaGeneratorService {
+
+    private final GeneratorContext generatorContext;
+
+    private final AvroSchemaMapper avroSchemaMapper;
+
+    private final FileProcessor fileProcessor;
+
+    @RunGenerator
+    public void run() {
+
+        var avroSchemas = avroSchemaMapper.apply(generatorContext);
+        avroSchemas.forEach(avroSchemaDTO -> {
+
+            var data = fileProcessor.processFreemarkerTemplate(AVRO_FREEMARKER_TEMPLATE_FILE, avroSchemaDTO.getFreemarkerVariables());
+            fileProcessor.generate(Map.of(avroSchemaDTO.getFileName(), data));
+        });
+    }
+
+}
---
+++ generator-common/src/main/java/ea/code/generator/service/freemarker/model/AvroSchema.java
@@ -0,0 +1,32 @@
+package ea.code.generator.service.freemarker.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class AvroSchema {
+
+    private String namespace;
+    private String name;
+    private String type;
+    private String logicalType;
+
+    private boolean isArray;
+
+    private List<String> enumValues = new ArrayList<>();
+
+    private Map<String, AvroSchema> fields = new HashMap<>();
+
+
+    public AvroSchema addField(String key,
+                               AvroSchema value) {
+
+        fields.put(key, value);
+        return this;
+    }
+
+}
